0a7838fa49fd009ffe8e90bfab7e42f2f689e1e7
Md. Mehedi Hasan <mehedihasan0937580@gmail.com>
2025-02-12 23:04:06 +0600

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const page = () => {
   return (
     <div>
-      <h1 className="font-serif text-center font-bold text-3xl my-5">
+      <h1 className="font-serif text-center font-bold text-3xl my-5 text-green-400 flex justify-center items-center h-[100vh]">
         contact page
       </h1>
     </div>
