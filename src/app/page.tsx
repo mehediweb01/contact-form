f0db53ad2ceee7c93d097e814909dd276a95ad62
Md. Mehedi Hasan <mehedihasan0937580@gmail.com>
2025-02-13 00:12:13 +0600

    page.tsx text updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ const page = () => {
   return (
     <div>
       <h1 className="font-serif text-center font-bold text-3xl my-5 text-green-400 flex justify-center items-center h-[100vh]">
-        contact page
+        Contact page | This page is unavailable 🪼 | Please wait for an update...
       </h1>
     </div>
   );
